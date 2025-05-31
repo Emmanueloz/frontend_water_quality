6c40a518f4d1c784e63ba7bd5a585cad5c6520d5
Fredev01 <fredev08@gmail.com>
2025-05-30 20:36:20 -0600

    feat(profile): pagina profile responsiva

--- lib/presentation/pages/profile.dart
+++ lib/presentation/pages/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend_water_quality/core/enums/screen_size.dart';
 import 'package:frontend_water_quality/presentation/widgets/layout/layout.dart';
 
 class Profile extends StatelessWidget {
@@ -9,112 +10,131 @@ class Profile extends StatelessWidget {
     return Layout(
         title: 'Perfil',
         builder: (context, screenSize) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 15),
-            child: Column(
+          if (screenSize == ScreenSize.mobile ||
+              screenSize == ScreenSize.tablet) {
+            return _MainContent(screenSize: screenSize);
+          }
+          return Center(child: _MainContent(screenSize: screenSize));
+        });
+  }
+}
+
+class _MainContent extends StatelessWidget {
+  final ScreenSize screenSize;
+  bool get isMobileOrTablet =>
+      screenSize == ScreenSize.mobile || screenSize == ScreenSize.tablet;
+
+  const _MainContent({
+    required this.screenSize,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 15),
+      child: Column(
+        children: [
+          // Card principal del perfil
+          Container(
+            width: isMobileOrTablet ? double.infinity : 800,
+            padding: const EdgeInsets.all(24),
+            decoration: BoxDecoration(
+              color: const Color(0xFF4A7C7A),
+              borderRadius:
+                  BorderRadius.vertical(top: Radius.circular(12)),
+            ),
+            child: Row(
               children: [
-                // Card principal del perfil
                 Container(
-                  width: double.infinity,
-                  padding: const EdgeInsets.all(24),
-                  decoration: BoxDecoration(
-                    color: const Color(0xFF4A7C7A),
-                    borderRadius:
-                        BorderRadius.vertical(top: Radius.circular(12)),
+                  width: 60,
+                  height: 60,
+                  decoration: const BoxDecoration(
+                    color: Colors.black,
+                    shape: BoxShape.circle,
                   ),
-                  child: Row(
-                    children: [
-                      Container(
-                        width: 60,
-                        height: 60,
-                        decoration: const BoxDecoration(
-                          color: Colors.black,
-                          shape: BoxShape.circle,
-                        ),
-                        child: const Icon(
-                          Icons.person,
-                          color: Color(0xFF7DD3C0),
-                          size: 30,
-                        ),
-                      ),
-                      const SizedBox(width: 20),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Marcos esp',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 24,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                          SizedBox(height: 8),
-                          Container(
-                            padding: EdgeInsets.symmetric(
-                              horizontal: 16,
-                              vertical: 6,
-                            ),
-                            decoration: BoxDecoration(
-                              color: Color(0xFF7DD3C0),
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                            child: Text(
-                              'cliente',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 14,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
+                  child: const Icon(
+                    Icons.person,
+                    color: Color(0xFF7DD3C0),
+                    size: 30,
                   ),
                 ),
-
-                // const SizedBox(height: 32),
-
-                // Información del usuario
-                Container(
-                  
-                  width: double.infinity,
-                  padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-                  decoration: BoxDecoration(
-                    color: const Color(0xFFf7fafa),
-                    borderRadius:
-                        BorderRadius.vertical(bottom: Radius.circular(12)),
-                  ),
-                  child: const Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      _ProfileInfoItem(
-                        label: 'Nombre de usuario',
-                        value: 'Marcos esp',
+                const SizedBox(width: 20),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Marcos esp',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                        fontWeight: FontWeight.w500,
                       ),
-                      SizedBox(height: 20),
-                      _ProfileInfoItem(
-                        label: 'Correo electrónico',
-                        value: 'mrlsp.452@gmail.com',
+                    ),
+                    SizedBox(height: 8),
+                    Container(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 6,
                       ),
-                      SizedBox(height: 20),
-                      _ProfileInfoItem(
-                        label: 'Teléfono',
-                        value: '919 453 2398',
+                      decoration: BoxDecoration(
+                        color: Color(0xFF7DD3C0),
+                        borderRadius: BorderRadius.circular(20),
                       ),
-                      SizedBox(height: 20),
-                      _ProfileInfoItem(
-                        label: 'Rol',
-                        value: 'cliente',
+                      child: Text(
+                        'cliente',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                        ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ],
             ),
-          );
-        });
+          ),
+    
+          // const SizedBox(height: 32),
+    
+          // Información del usuario
+          Container(
+            
+            width: isMobileOrTablet ? double.infinity : 800,
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+            decoration: BoxDecoration(
+              color: const Color(0xFFf7fafa),
+              borderRadius:
+                  BorderRadius.vertical(bottom: Radius.circular(12)),
+            ),
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _ProfileInfoItem(
+                  label: 'Nombre de usuario',
+                  value: 'Marcos esp',
+                ),
+                SizedBox(height: 20),
+                _ProfileInfoItem(
+                  label: 'Correo electrónico',
+                  value: 'mrlsp.452@gmail.com',
+                ),
+                SizedBox(height: 20),
+                _ProfileInfoItem(
+                  label: 'Teléfono',
+                  value: '919 453 2398',
+                ),
+                SizedBox(height: 20),
+                _ProfileInfoItem(
+                  label: 'Rol',
+                  value: 'cliente',
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
 
