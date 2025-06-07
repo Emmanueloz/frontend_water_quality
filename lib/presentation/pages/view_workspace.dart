8afeb9dffdfff5780cf3ee001054f14cb44af66e
Fredev01 <fredev08@gmail.com>
2025-06-06 22:28:13 -0600

    feat(alerts): integracion de la ruta de alertas en el sidebar de viewWorkspace.

--- lib/presentation/pages/view_workspace.dart
+++ lib/presentation/pages/view_workspace.dart
@@ -3,6 +3,8 @@ import 'package:frontend_water_quality/core/enums/list_workspaces.dart';
 import 'package:frontend_water_quality/core/interface/navigation_item.dart';
 import 'package:frontend_water_quality/presentation/widgets/layout/layout.dart';
 import 'package:frontend_water_quality/presentation/widgets/specific/workspace/main_workspace.dart';
+import 'package:frontend_water_quality/router/routes.dart';
+import 'package:go_router/go_router.dart';
 
 class ViewWorkspace extends StatelessWidget {
   final String id;
@@ -17,6 +19,13 @@ class ViewWorkspace extends StatelessWidget {
         print("Navigate to meters");
       } else if (index == 1) {
         // Navigate to alerts
+        context.goNamed(
+          Routes.alerts.name,
+          pathParameters: {
+            "id": id,
+            "type": type.name,
+          },
+        );
         print("Navigate to alerts");
       } else if (index == 2) {
         // Navigate to guests
