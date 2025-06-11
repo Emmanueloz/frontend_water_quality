1a76716f41fb8aa9d8708ed41aabc836cd193670
shado <mraul7875@gmail.com>
2025-06-10 20:43:19 -0600

    feat: Implementacion de la separacion del codigo de la ilustracion de la imagen en login

--- lib/presentation/pages/login.dart
+++ lib/presentation/pages/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:frontend_water_quality/presentation/widgets/layout/layout.dart';
+import 'package:frontend_water_quality/presentation/widgets/specific/auth/illustration_section.dart';
 import 'package:frontend_water_quality/presentation/widgets/specific/form_login.dart';
 
 class LoginPage extends StatelessWidget {
@@ -39,47 +40,21 @@ class LoginPage extends StatelessWidget {
                 ),
                 child: Row(
                   children: [
-                    Expanded(child: _IllustrationSection()),
                     Expanded(
                       child: Padding(
                         padding: const EdgeInsets.symmetric(horizontal: 50),
                         child: const LoginForm(),
                       ),
                     ),
+                    Expanded(
+                      child: IllustrationSection(),
+                    )
                   ],
                 ),
               );
             }
           },
         ),
-      ),
-    );
-  }
-}
-
-class _IllustrationSection extends StatelessWidget {
-  final bool isMobile;
-  const _IllustrationSection({this.isMobile = false});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(24),
-      decoration: BoxDecoration(
-        color: const Color(0xFFEAEAEA),
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(isMobile ? 16 : 16),
-          bottomLeft: Radius.circular(isMobile ? 0 : 16),
-          topRight: Radius.circular(isMobile ? 16 : 0),
-        ),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset('../../../assets/images/agua.png',
-              width: 500, height: 500),
-          const SizedBox(height: 20),
-        ],
       ),
     );
   }
