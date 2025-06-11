cc078f108d6a24429a2b73a5a06f3026fbc1e175
EmmanuelOZ <davidozuna84@hotmail.com>
2025-06-10 22:22:41 -0600

    refactor: usar inputDecorationTheme por defecto a los inputs de toda la aplicación

--- lib/presentation/widgets/specific/profile/profile_info_field.dart
+++ lib/presentation/widgets/specific/profile/profile_info_field.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:frontend_water_quality/core/constants/profile_ui.dart';
 
 class ProfileInfoField extends StatelessWidget {
   final String label;
@@ -15,55 +14,12 @@ class ProfileInfoField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 7),
-          child: Text(
-            label,
-            style: theme.textTheme.titleSmall?.copyWith(
-              color: theme.colorScheme.secondary, 
-              fontSize: 14,
-              fontWeight: FontWeight.w300,
-            ),
-          ),
-        ),
-        const SizedBox(height: 4),
-        TextFormField(
-              keyboardType: keyboardType,
-              initialValue: value,
-              decoration: InputDecoration(
-                isDense: true,
-                border: UnderlineInputBorder(
-                  borderRadius: BorderRadius.circular(ProfileConstants.fieldBorderRadius),
-                  borderSide: BorderSide(color: theme.colorScheme.primary, width: 1,),
-                ),
-                enabledBorder: UnderlineInputBorder(
-                  borderRadius: BorderRadius.circular(ProfileConstants.fieldBorderRadius),
-                  borderSide: BorderSide(color: theme.colorScheme.primary, width: 1,),
-                ),
-                // filled: true,
-                // fillColor: const Color(0xFFEFEFEF),
-                contentPadding: EdgeInsets.symmetric(vertical: 12, horizontal: 8),
-
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(ProfileConstants.fieldBorderRadius),
-                  borderSide: BorderSide(color: theme.colorScheme.primary, width: 1),
-                  ),
-                labelStyle: theme.textTheme.titleSmall?.copyWith(
-                  color: theme.colorScheme.secondary,
-                  fontSize: 14,
-                  fontWeight: FontWeight.w300,
-                ),
-              ),
-              style: theme.textTheme.bodyMedium?.copyWith(
-                color: theme.colorScheme.secondary, // Color del texto del valor
-              ),
-            ),
-      ],
+    return TextFormField(
+      keyboardType: keyboardType,
+      initialValue: value,
+      decoration: InputDecoration(
+        labelText: label,
+      ),
     );
   }
 }
