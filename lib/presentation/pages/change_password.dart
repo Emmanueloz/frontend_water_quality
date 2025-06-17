1883a58462e44a96caf7e192bc908f94726285c4
shado <mraul7875@gmail.com>
2025-06-17 13:00:54 -0600

    feat/ CAmbios en change, quitado de card y layoudbuilder

--- lib/presentation/pages/change_password.dart
+++ lib/presentation/pages/change_password.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:frontend_water_quality/presentation/widgets/layout/layout.dart';
 import 'package:frontend_water_quality/presentation/widgets/specific/form_change.dart';
-import 'package:go_router/go_router.dart';
+import 'package:frontend_water_quality/router/routes.dart';
 
 class ChangePasswordPage extends StatelessWidget {
   final String title;
@@ -17,33 +18,22 @@ class ChangePasswordPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Layout(
       title: title,
-      builder: (context, screenSize) => Center(
-        child: LayoutBuilder(
-          builder: (context, constraints) {
-            final isMobile = constraints.maxWidth < 800;
-            return Padding(
-              padding: const EdgeInsets.all(24),
-              child: ConstrainedBox(
-                constraints: BoxConstraints(
-                  maxWidth: isMobile ? double.infinity : 600,
-                  maxHeight: 500,
-                ),
-                child: Card(
-                  elevation: 4,
-                  child: Padding(
-                    padding: const EdgeInsets.all(32.0),
-                    child: ChangePasswordForm(
-                      email: email,
-                      onSubmit: (newPassword) {
-                        // Simulación de cambio de contraseña
-                        context.goNamed('login');
-                      },
-                    ),
-                  ),
-                ),
-              ),
-            );
-          },
+      builder: (context, screenSize) => Align(
+        alignment: Alignment.center,
+        child: Padding(
+          padding: const EdgeInsets.all(24),
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(
+              maxWidth: 600,
+              maxHeight: 500,
+            ),
+            child: ChangePasswordForm(
+              email: email,
+              onSubmit: (newPassword) {
+                context.goNamed(Routes.login.name);
+              },
+            ),
+          ),
         ),
       ),
     );
