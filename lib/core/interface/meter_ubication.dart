44858659d87a9ee42033685f451ce911e06ff6a0
Fredev01 <fredev08@gmail.com>
2025-06-21 19:49:28 -0600

    feat: clase para las coordenadas del medidor

---
+++ lib/core/interface/meter_ubication.dart
@@ -0,0 +1,7 @@
+class MeterUbication {
+  final String name;
+  final double latitude;
+  final double longitude;
+
+  MeterUbication({required this.name, required this.latitude, required this.longitude});
+}
