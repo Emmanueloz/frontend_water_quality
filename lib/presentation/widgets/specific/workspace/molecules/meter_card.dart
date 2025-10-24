041b16390ed4deed06dde4e2e1f952a11d442ded
Fredev01 <fredev08@gmail.com>
2025-10-23 21:04:31 -0600

    feat: Refactor MeterCard to use BaseCard

--- lib/presentation/widgets/specific/workspace/molecules/meter_card.dart
+++ lib/presentation/widgets/specific/workspace/molecules/meter_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:frontend_water_quality/domain/models/meter_model.dart';
+import 'package:frontend_water_quality/presentation/widgets/common/molecules/base_card.dart';
 
 class MeterCard extends StatelessWidget {
   final String id;
@@ -34,97 +35,13 @@ class MeterCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(12), // esquinas redondeadas
-      ),
-      clipBehavior: Clip.antiAlias, // para que el borde se recorte bien
-      child: InkWell(
-        onTap: onTap,
-        child: Container(
-          decoration: BoxDecoration(
-            color: theme.colorScheme.secondary.withValues(alpha: 0.48),
-            border: Border(
-              left: BorderSide(
-                color: theme.colorScheme.tertiary,
-                width: 10,
-              ),
-            ),
-          ),
-          // padding: const EdgeInsets.all(8),
-          child: Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 18.0, horizontal: 20.0),
-            child: Stack(
-              children: [
-                // Contenido principal (columna con título y ubicación)
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      name,
-                      style: theme.textTheme.titleLarge?.copyWith(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(height: 14),
-                    Text(
-                      'Ubicación:',
-                      style: theme.textTheme.bodyLarge?.copyWith(
-                        color: theme.colorScheme.onPrimary.withAlpha(185),
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    if (location.nameLocation != null)
-                      Text(
-                        location.nameLocation!,
-                        style: theme.textTheme.bodyMedium?.copyWith(
-                          height: 1.25,
-                        ),
-                        maxLines: 3,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    // espacio flexible
-                    const SizedBox(height: 6),
-                  ],
-                ),
-
-                // Status pill (esquina superior derecha)
-                Positioned(
-                  right: 0,
-                  top: 0,
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-                    // margin: const EdgeInsets.only(top: 6),
-                    decoration: BoxDecoration(
-                      color: theme.colorScheme.primary.withValues(alpha: 0.62),
-                      borderRadius: BorderRadius.circular(19),
-                    ),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      spacing: 6,
-                      children: [
-                        Text(
-                          state,
-                          style: theme.textTheme.bodyMedium?.copyWith(
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                        Icon(
-                            _statusIcon,
-                            size: 18,
-                            color: theme.colorScheme.tertiary,
-                          ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+    return BaseCard(
+      title: name,
+      tag: "Ubicación",
+      subtitle: location.nameLocation,
+      icon: _statusIcon,
+      onTap: onTap,
+      state: state,
     );
   }
 }
